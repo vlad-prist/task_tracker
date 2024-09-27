15b722e110cd6ce5fbca1299cb2d4466f36c23ed
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-09-27 12:41:53 +0300

    изменение и "причесывание" урлов

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,22 +1,39 @@
 from django.urls import path
 from tracker.apps import TrackerConfig
-from tracker.views import TaskViewSet, EmployeeViewSet, EmpListAPIView, TaskOrderingListAPIView, TaskEmptyListAPIView, EmployeesBusy, EmpsFreeForTask, EmployeeTaskListAPIView
+from tracker.views import (
+    TaskViewSet,
+    EmployeeViewSet,
+    TaskListAPIView,
+    ImportantTaskList,
+    EmployeeTaskListAPIView,
+)
 from rest_framework.routers import DefaultRouter
-
+from tracker.models import Task
 
 app_name = TrackerConfig.name
 
 
 router = DefaultRouter()
-router.register(r'task', TaskViewSet, basename='task')
-router.register(r'employee', EmployeeViewSet, basename='employee')
+router.register(r"task", TaskViewSet, basename="task")
+router.register(r"employee", EmployeeViewSet, basename="employee")
 
 urlpatterns = [
-    path('task/list/', TaskOrderingListAPIView.as_view(), name='tasks_list'),
-    path('task/empty/', TaskEmptyListAPIView.as_view(), name='task-empty'),
-
-    path('employee/list/', EmpListAPIView.as_view(), name='employees_list'),
-    path('employee/busy/', EmployeesBusy.as_view(), name='employees_busy'),
-    path('employee/free/', EmpsFreeForTask.as_view(), name='employees_free'),
-    path('employee/taskbusy/', EmployeeTaskListAPIView.as_view(), name='employees_task_busy'),
+    # Запрашивает из БД список всех задач.
+    path("task/list/", TaskListAPIView.as_view(), name="tasks_list"),
+    # Запрашивает из БД список задач без сотрудника.
+    path(
+        "task/list/empty/",
+        TaskListAPIView.as_view(queryset=Task.objects.filter(employee=None)),
+        name="task_list_empty",
+    ),
+    # Запрашивает из БД список всех занятых сотрудников.
+    path(
+        "employee/busy/", EmployeeTaskListAPIView.as_view(), name="employees_task_busy"
+    ),
+    # Запрашивает из БД список сотрудников для задач с высоким приоритетом.
+    path(
+        "employee/available/",
+        ImportantTaskList.as_view(),
+        name="employees_available_list",
+    ),
 ] + router.urls
