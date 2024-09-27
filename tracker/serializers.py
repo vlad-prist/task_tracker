932d159f5ae7e8c2d2564f1f345dedfdce39b493
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-09-27 12:26:12 +0300

    "причесывание" сериализаторов

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -5,12 +5,23 @@ from tracker.validators import validate_deadline, StatusValidator
 
 class TaskSerializer(serializers.ModelSerializer):
     """ Список всех задач """
-    deadline = serializers.DateTimeField(format='%d.%m.%Y %H:%M', input_formats=['%d.%m.%Y %H:%M'],
-                                         validators=[validate_deadline])
+    deadline = serializers.DateTimeField(format='%d.%m.%Y %H:%M',
+                                         input_formats=['%d.%m.%Y %H:%M'],
+                                         validators=[validate_deadline]
+                                         )
 
     class Meta:
         model = Task
-        fields = ("id", "title", "related_task", "description", "employee", "deadline", "status",)
+        fields = (
+            "id",
+            "title",
+            "related_task",
+            "description",
+            "employee",
+            "deadline",
+            "priority",
+            "status",
+        )
         validators = [StatusValidator(field_emp="employee", field_status="status"),]
 
     def update(self, obj, validated_data):
@@ -23,36 +34,28 @@ class TaskSerializer(serializers.ModelSerializer):
 
 class TaskShortListSerializer(serializers.ModelSerializer):
     """ Краткая информация о задачах. """
-    deadline = serializers.DateTimeField(format='%d.%m.%Y %H:%M', input_formats=['%d.%m.%Y %H:%M'],
-                                         validators=[validate_deadline])
+    deadline = serializers.DateTimeField(format='%d.%m.%Y %H:%M',
+                                         input_formats=['%d.%m.%Y %H:%M'],
+                                         validators=[validate_deadline]
+                                         )
 
     class Meta:
         model = Task
-        fields = ("id", "title", "related_task", "description", "deadline", "status",)
+        fields = ("id",
+                  "title",
+                  "related_task",
+                  "description",
+                  "deadline",
+                  "priority",
+                  "status",
+                  )
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
-    """ Список всех сотрудников """
+    """ Список всех сотрудников. """
     list_tasks = TaskShortListSerializer(source='task', many=True)
     total_tasks_count = serializers.SerializerMethodField()
     active_tasks_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Employee
-        fields = ("id", "name", "position", "department", "active_tasks_count", "total_tasks_count", "list_tasks")
-
-    def get_total_tasks_count(self, obj):
-        return obj.task.count()
-
-    def get_active_tasks_count(self, obj):
-        return obj.task.filter(status=Task.STATUS_IN_PROGRESS).count()
-
-
-class EmployeeActiveTaskSerializer(serializers.ModelSerializer):
-    #working
-    # list_tasks = TaskShortListSerializer(source='task', many=True)
-    active_tasks_count = serializers.SerializerMethodField()
-    active_tasks_list = serializers.SerializerMethodField()
 
     class Meta:
         model = Employee
@@ -62,16 +65,49 @@ class EmployeeActiveTaskSerializer(serializers.ModelSerializer):
             "position",
             "department",
             "active_tasks_count",
+            "total_tasks_count",
+            "list_tasks",
+        )
+
+    def get_total_tasks_count(self, obj):
+        """ Общие число задач. """
+        return obj.task.count()
+
+    def get_active_tasks_count(self, obj):
+        """ Задачи в работе. """
+        return obj.task.filter(status=Task.STATUS_IN_PROGRESS).count()
+
+
+class EmployeeActiveTaskSerializer(serializers.ModelSerializer):
+    #working
+    """ Список сотрудников с активными задачами. """
+    active_tasks_count = serializers.SerializerMethodField()
+    active_tasks_list = serializers.SerializerMethodField()
+    total_tasks_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Employee
+        fields = (
+            "id",
+            "name",
+            "position",
+            "department",
+            "total_tasks_count",
+            "active_tasks_count",
             "active_tasks_list",
         )
 
     def get_active_tasks_count(self, obj):
+        """ Задачи в работе. """
         return obj.task.filter(status=Task.STATUS_IN_PROGRESS).count()
 
+    def get_total_tasks_count(self, obj):
+        """ Общие число задач. """
+        return obj.task.count()
+
     def get_active_tasks_list(self, obj):
+        """ Список задач в работе. """
         data = obj.task.filter(status=Task.STATUS_IN_PROGRESS)
-        # data = Task.objects.filter(status=Task.STATUS_IN_PROGRESS, employee=obj)
-        # print(data)
         new_data = TaskShortListSerializer(data=data, many=True)
         new_data.is_valid()
         return new_data.data
