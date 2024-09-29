26295b015b8e55ac9e8b1f21a27a4ca72d339051
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-09-29 13:04:06 +0300

    Тестирование модели Task

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,9 +1,13 @@
+# import datetime
+from datetime import timedelta, datetime
+
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 from rest_framework.test import APITestCase
 from rest_framework import status
 from tracker.models import Employee, Task
-
+from tracker.validators import validate_deadline
 
 class EmployeeTestCase(APITestCase):
     """ Тест экземпляра модели Employee. """
@@ -32,7 +36,7 @@ class EmployeeTestCase(APITestCase):
             'department': 'HR'
         }
         response = self.client.post(url, data=data)
-        print(response)
+        # print(response.json())
         self.assertEqual(
             response.status_code, status.HTTP_201_CREATED
         )
@@ -78,4 +82,159 @@ class EmployeeTestCase(APITestCase):
         )
         self.assertEqual(
             len(data['results']), Employee.objects.all().count()
+        )
+
+
+class TaskTestCase(APITestCase):
+    """ Тест экземпляра модели Task. """
+
+    def setUp(self):
+        self.task_one = Task.objects.create(
+            title="Task one",
+            description="This is a test task",
+            deadline=timezone.now() + timedelta(days=1),
+            status=Task.STATUS_CREATED,
+        )
+        self.employee = Employee.objects.create(name='Иван Иванов', position='Разработчик', department='IT')
+
+    def test_task_retrieve(self):
+        """ Тест на получение задачи по ПК. """
+        url = reverse("tracker:task-detail", args=(self.task_one.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data['title'], self.task_one.title
+        )
+
+    def test_task_create(self):
+        """ Тест на создание новой задачи. """
+        url = reverse("tracker:task-list")
+        deadline = timezone.now() + timedelta(days=1)
+        data = {
+            'title': 'Task two',
+            'description': 'This is another test task',
+            'deadline': deadline.strftime('%d.%m.%Y %H:%M'),
+            'employee': self.employee.pk,
+            'status': Task.STATUS_IN_PROGRESS,
+            'priority': 'high',
+        }
+        response = self.client.post(url, data=data)
+        # print(response.json())
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Task.objects.all().count(), 2
+        )
+
+    def test_task_update(self):
+        """  Тест на обновление задачи. """
+        url = reverse('tracker:task-detail', args=(self.task_one.pk,))
+        data = {
+            'title': "Task one updated",
+            'description': 'This is a test task',
+            'deadline': (timezone.now() + timedelta(days=1)).strftime('%d.%m.%Y %H:%M'),
+            'employee': self.employee.pk,
+            'priority': 'medium',
+            'status': Task.STATUS_IN_PROGRESS,
+        }
+        response = self.client.put(url, data=data)
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            Task.objects.get(pk=self.task_one.pk).priority, 'medium'
+        )
+
+    def test_task_partial_update(self):
+        """ Тест на частичное обновление задачи. """
+        url = reverse('tracker:task-detail', args=(self.task_one.pk,))
+        data = {
+            'title': 'Task one partial updated',
+        }
+        response = self.client.patch(url, data=data)
+        # print(response.json())
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            Task.objects.get(pk=self.task_one.pk).title, 'Task one partial updated'
+        )
+
+    def test_task_delete(self):
+        """ Тест на удаление задачи. """
+        url = reverse('tracker:task-detail', args=(self.task_one.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT,
+        )
+        self.assertEqual(
+            Task.objects.all().count(), 0
+        )
+
+    def test_task_list(self):
+        """ Тест вывод списка всех экземпляров модели Task. """
+        url = reverse('tracker:task-list')
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            len(data['results']), Task.objects.all().count()
+        )
+
+    def test_change_status(self):
+        """ Тест на изменение статуса задачи при назначении сотрудника на задачу. """
+        url = reverse('tracker:task-detail', args=(self.task_one.pk,))
+        data = {
+            'employee': self.employee.pk,
+        }
+        response = self.client.patch(url, data=data)
+        # print(response.json())
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            Task.objects.get(pk=self.task_one.pk).status, Task.STATUS_IN_PROGRESS
+        )
+
+    def test_validation_deadline(self):
+        """ Тест валидации дедлайна. """
+        url = reverse("tracker:task-list")
+        deadline = timezone.now() - timedelta(days=1)
+        data = {
+            'title': 'Task three',
+            'description': 'This is yet another test task',
+            'deadline': deadline.strftime('%d.%m.%Y %H:%M'),
+            'employee': self.employee.pk,
+            'status': Task.STATUS_IN_PROGRESS,
+            'priority': 'high',
+        }
+        response = self.client.post(url, data=data)
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST
+        )
+        # self.assertEqual(
+        #     response.json()['deadline'][0], f'{validate_deadline(deadline)}'
+        # )
+
+    def test_overdue_task(self):
+        """ Тест на проверку просроченной задачи. """
+        url = reverse('tracker:task-list')
+        data = {
+            'title': 'Task four',
+            'description': 'This is another test task with overdue status',
+            'deadline': (timezone.now() + timedelta(days=1)).strftime('%d.%m.%Y %H:%M'),
+            'employee': self.employee.pk,
+            'status': Task.STATUS_OVERDUE,
+            'priority': 'high',
+        }
+        response = self.client.post(url, data=data)
+        # print(response.json())
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST
         )
